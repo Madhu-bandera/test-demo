89b88baff07b0a76a271a3d70a4ea3e3fa3bfcd3
madhu <bamadhu1995@gmail.com>
2021-10-01 20:43:52 +0530

    routing admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { StudentComponent } from './student/student.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = 
 [
@@ -12,6 +13,8 @@ const routes: Routes =
   component:TeacherComponent},
   {path:"dashboard",
   component:DashboardComponent},
+  {path:"admin",
+  component:AdminComponent},
   {
     path: '',
     pathMatch: 'full',
