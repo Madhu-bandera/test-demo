9d850d89a0d1a007373d6b87d6fd8001b63c2dfd
madhu <bamadhu1995@gmail.com>
2021-10-04 20:45:34 +0530

    import app module ts

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-teacher',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeacherComponent implements OnInit {
 
-  constructor() { }
+  myTeacher!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.myTeacher=this.fb.group({
+      name:["",Validators.required ],
+      age:["",Validators.required ],
+      email:["",[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')] ],
+      phone:["",Validators.required ],
+      exp:["",Validators.required ],
+      lang:["",Validators.required ],
+      address:["",Validators.required ],
+      state:["",Validators.required ],
+      pincode:["",[Validators.required,Validators.maxLength(6)] ],
+    
+    })
   }
 
+  Save(){
+    console.log("form",this.myTeacher.controls.Email.valid )
+    if(this.myTeacher.valid){
+      alert("Name"+this.myTeacher.value.name)
+    }
+    else{
+      alert("Please fill required fields")
+    }
+  }
 }
