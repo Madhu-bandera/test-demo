13c87785d5a8ce9873a4936a91b418f40d26d94d
madhu <bamadhu1995@gmail.com>
2021-10-04 20:40:48 +0530

    import from app module

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-@Component({
+
+ @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+myStudent!:FormGroup;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.myStudent=this.fb.group({
+      name:["",Validators.required ],
+      id:["",Validators.required],
+      DOB:["",Validators.required],
+      Email:["",Validators.required],
+      Phone:["",Validators.required],
+      Class:["",Validators.required],
+      Address:["",Validators.required],
+      State:["",Validators.required],
+      pincode:["",Validators.required],
+
+
+    })
   }
 
+  Save(){
+    console.log("form",this.myStudent.value)
+    if(this.myStudent.valid){
+      alert("Name"+this.myStudent.value)
+    }
+    else{
+      alert("Please fill required fields")
+    }
+  }
 }
