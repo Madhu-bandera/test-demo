b803a13907b0fde0c2787e932e5ba876f8489a67
madhu <bamadhu1995@gmail.com>
2021-10-06 11:14:49 +0530

    learning ts

--- src/app/form-example/form-example.component.ts
+++ src/app/form-example/form-example.component.ts
@@ -4,25 +4,29 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-form-example',
   templateUrl: './form-example.component.html',
-  styleUrls: ['./form-example.component.scss']
+  styleUrls: ['./form-example.component.scss'],
 })
 export class FormExampleComponent implements OnInit {
-  myForm!:FormGroup;
-  constructor(private fb:FormBuilder) { }
+  myForm!: FormGroup;
+
+  city: any;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm=this.fb.group({
-      name:["",Validators.required],
-      phoneNo:""
-    })
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      phoneNo: '',
+    });
   }
-  submit(){
-    console.log("form",this.myForm.value)
-    if(this.myForm.valid){
-      alert("Name"+this.myForm.value)
+  submit() {
+    console.log('form', this.myForm.value);
+    if (this.city.length > 0) {
+      alert('City ' + this.city);
     }
-    else{
-      alert("Please fill required fields")
-    }
+    // if (this.myForm.valid) {
+    //   alert('Name' + this.myForm.value);
+    // } else {
+    //   alert('Please fill required fields');
+    // }
   }
 }
