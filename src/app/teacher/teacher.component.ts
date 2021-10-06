c7d80b79189b04d0c6226e9184ef321256695faf
madhu <bamadhu1995@gmail.com>
2021-10-06 11:24:46 +0530

    add email validation

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -16,18 +16,19 @@ export class TeacherComponent implements OnInit {
       name:["",Validators.required ],
       age:["",Validators.required ],
       email:["",[Validators.required,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')] ],
-      phone:["",Validators.required ],
+      phone:["",[Validators.required,Validators.maxLength(10)] ],
       exp:["",Validators.required ],
       lang:["",Validators.required ],
       address:["",Validators.required ],
       state:["",Validators.required ],
       pincode:["",[Validators.required,Validators.maxLength(6)] ],
+      website:["",[Validators.required,Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?')] ],  
     
     })
   }
 
   Save(){
-    console.log("form",this.myTeacher.controls.Email.valid )
+    console.log("form",this.myTeacher.controls.website.valid)
     if(this.myTeacher.valid){
       alert("Name"+this.myTeacher.value.name)
     }
