8c2f15c726a34dda3c94f8d9fad7f7f2ab69be6f
madhu <bamadhu1995@gmail.com>
2021-10-27 17:54:29 +0530

    bugs fixed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  remove() {
+    alert('Are you sure you want to logout?');
+    localStorage.removeItem('x-auth');
+    localStorage.removeItem('userInfo');
+    this.router.navigateByUrl('/login');
   }
-
 }
